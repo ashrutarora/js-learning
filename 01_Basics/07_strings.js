a822acea8946957414dedf6d1b0dd2d87cc19f99
ashrut1011 <ashrutarora@gmail.com>
2024-06-15 09:48:42 +0530

    string operations

---
+++ 01_Basics/07_strings.js
@@ -0,0 +1,16 @@
+const name = "Ashrut";
+const repoCount = 50;
+
+// Outdated Syntax
+// console.log(name + repoCount);
+
+// Current and Latest Syntax
+console.log(`Hello my name is ${name} and I have ${repoCount} repos`);
+
+const gameName = new String("Ashrut");
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(1));
+console.log(gameName.indexOf("b"));
+console.log(gameName.indexOf("A"));
+
+// Refer to MDN Docs for more
